48e72175e21b1b4a9d8c3c6c99caff7982dbc2ca
thushan-godahewa <thushan.godahewa@gmail.com>
2026-03-29 15:51:17 +1100

    First commit of MCP server using Java and Spring Boot

---
+++ src/main/java/dev/tsg/demos/mycpserver/MycpserverApplication.java
@@ -0,0 +1,13 @@
+package dev.tsg.demos.mycpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycpserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycpserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tsg/demos/mycpserver/MycpserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tsg.demos.mycpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycpserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
