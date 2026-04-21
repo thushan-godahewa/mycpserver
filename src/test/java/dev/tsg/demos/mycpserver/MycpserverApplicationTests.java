cec9d576c6967010f363fa6ec99c1ed38ccf75ff
thushan-godahewa <thushan.godahewa@gmail.com>
2026-04-21 10:01:23 +1000

    Removed Greeting resource implementation.

--- src/test/java/dev/tsg/demos/mycpserver/MycpserverApplicationTests.java
+++ src/test/java/dev/tsg/demos/mycpserver/MycpserverApplicationTests.java
@@ -2,12 +2,8 @@ package dev.tsg.demos.mycpserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-
-import dev.tsg.demos.mycpserver.model.GreetingRequest;
 
 @SpringBootTest
-@ContextConfiguration(classes = {GreetingRequest.class})
 class MycpserverApplicationTests {
 
 	@Test
